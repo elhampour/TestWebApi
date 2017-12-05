f12fec7717273f7af39e90ed9fbec0b43692183d
Ehsan Elhampour <ehsan.elhampour@gmail.com>
2017-12-05 09:44:49 +0330

    init

---
+++ TestWebApi.Api/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using WebApi.AuthenticationFilter;
+
+namespace TestWebApi.Api.Controllers
+{
+    public class AuthenticationFilter: AuthorizeAttribute
+    {
+        public override void OnAuthorization(HttpActionContext context)
+        {
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+
+    public class ProductController : ApiController
+    {
+        #region Get
+
+        [HttpGet]
+        [AuthenticationFilter]
+        public HttpResponseMessage Get()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id=1,Title="Product1" },
+                new Product() {Id=2,Title="Product2" }
+            };
+            return Request.CreateResponse(HttpStatusCode.OK, new { products = products });
+        }
+
+        #endregion
+    }
+}
